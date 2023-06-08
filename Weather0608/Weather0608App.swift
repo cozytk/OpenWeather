10ec3913a5c1c46a60f8a8802b6f84068ce796ac
Taekyun Kim <taekkim@student.42seoul.kr>
2023-06-08 10:46:05 +0900

    Initial Commit

---
+++ Weather0608/Weather0608App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather0608App.swift
+//  Weather0608
+//
+//  Created by taekkim on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct Weather0608App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
